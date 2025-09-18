4525c0799ca924d32854e8d6ce6cae586be6a922
Daniel Scally <44758401+dascally@users.noreply.github.com>
2025-09-18 13:02:01 -0400

    NumbersEndpoints: Implement prime

--- API Challenges/Endpoints/NumbersEndpoints.cs
+++ API Challenges/Endpoints/NumbersEndpoints.cs
@@ -26,5 +26,18 @@ public static class NumberGamesEndpoints
 
             return sb.ToString();
         });
+
+        app.MapGet("/numbers/prime/{number}", (int number) =>
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        });
     }
 }
