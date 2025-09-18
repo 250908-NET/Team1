651978cf5d44ab01fe0acaeddf707f1c7598140a
Daniel Scally <44758401+dascally@users.noreply.github.com>
2025-09-18 16:09:40 -0400

    UnitConverter: Implement volume conversions

--- API Challenges/Endpoints/UnitConverterEndpoints.cs
+++ API Challenges/Endpoints/UnitConverterEndpoints.cs
@@ -11,7 +11,7 @@ public static class UnitConverterEndpoints
             { "feet", new Dictionary<string, double> {
                 { "meters", 0.3048 },
                 { "feet", 1.0 },
-                { "inches", 12 }
+                { "inches", 12.0 }
             } },
             { "inches", new Dictionary<string, double> {
                 { "meters", 0.0254 },
@@ -40,6 +40,25 @@ public static class UnitConverterEndpoints
             } }
         };
 
+    private static Dictionary<string, Dictionary<string, double>> volumeConversionFactors =
+        new Dictionary<string, Dictionary<string, double>>()
+        {
+            { "liters", new Dictionary<string, double> {
+                { "liters", 1.0 },
+                { "gallons", 0.264172 },
+                { "cups", 4.226753 }
+            } },
+            { "gallons", new Dictionary<string, double> {
+                { "liters", 3.785412 },
+                { "gallons", 1.0 },
+                { "cups", 16.0 }
+            } },
+            { "cups", new Dictionary<string, double> {
+                { "liters", 0.2365882 },
+                { "gallons", 0.0625 },
+                { "cups", 1.0 }
+            } }
+        };
 
     public static void MapUnitConverterEndpoints(this IEndpointRouteBuilder app)
     {
@@ -155,6 +174,64 @@ public static class UnitConverterEndpoints
             }
             if (factor == 0.0)
                 return Results.BadRequest("Unsupported unit. Supported units are: kg, lbs, ounces.");
+
+            return Results.Ok(input * factor);
+        });
+
+        app.MapGet("/convert/volume/{value}/{fromUnit}/{toUnit}", (string value, string fromUnit, string toUnit) =>
+        {
+            double input;
+            if (!double.TryParse(value, out input))
+                return Results.BadRequest("Invalid value to convert. Please enter a number.");
+
+            double factor = 0.0;
+            switch (fromUnit.ToLower())
+            {
+                case "liters":
+                    switch (toUnit.ToLower())
+                    {
+                        case "liters":
+                            factor = volumeConversionFactors["liters"]["liters"];
+                            break;
+                        case "gallons":
+                            factor = volumeConversionFactors["liters"]["gallons"];
+                            break;
+                        case "cups":
+                            factor = volumeConversionFactors["liters"]["cups"];
+                            break;
+                    }
+                    break;
+                case "gallons":
+                    switch (toUnit.ToLower())
+                    {
+                        case "liters":
+                            factor = volumeConversionFactors["gallons"]["liters"];
+                            break;
+                        case "gallons":
+                            factor = volumeConversionFactors["gallons"]["gallons"];
+                            break;
+                        case "cups":
+                            factor = volumeConversionFactors["gallons"]["cups"];
+                            break;
+                    }
+                    break;
+                case "cups":
+                    switch (toUnit.ToLower())
+                    {
+                        case "liters":
+                            factor = volumeConversionFactors["cups"]["liters"];
+                            break;
+                        case "gallons":
+                            factor = volumeConversionFactors["cups"]["gallons"];
+                            break;
+                        case "cups":
+                            factor = volumeConversionFactors["cups"]["cups"];
+                            break;
+                    }
+                    break;
+            }
+            if (factor == 0.0)
+                return Results.BadRequest("Unsupported unit. Supported units are: liters, gallons, cups.");
 
             return Results.Ok(input * factor);
         });
